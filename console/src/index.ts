a6ae7a9d7f1a8043ec0b1fcf4e272fed492b5aec
li-guohao <git@liguohao.cn>
2023-06-02 13:49:50 +0800

    feat: finish example view and index.ts

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,6 +1,33 @@
-// export default definePlugin({
-//     components: {},
-//     routes: [],
-//     extensionPoints: {},
-// });
+import { definePlugin } from "@runikaros/shared"
+import HomeView from "./views/HomeView.vue";
+
 console.log('Hello Ikaros')
+export default definePlugin({
+    name: 'PluginStater',
+    components: {},
+    routes: [
+      {
+        parentName: "Root",
+        route: {
+          path: "/example",
+          children: [
+            {
+              path: "",
+              name: "Example",
+              component: HomeView,
+              meta: {
+                title: "示例页面",
+                searchable: true,
+                menu: {
+                  name: "示例页面",
+                  group: "示例分组",
+                  priority: 0,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+    extensionPoints: {},
+})
---
+++ console/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">Hello Ikaros Plugin Stater Home View.</div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: auto;
+}
+</style>
