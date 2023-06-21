bf6bf2cd9f120349fbb3e65fd61e30ec87d08252
li-guohao <git@liguohao.cn>
2023-06-21 09:27:53 +0800

    update: add a name field in ConfigCustom.

--- src/main/java/run/ikaros/plugin/starter/ConfigCustom.java
+++ src/main/java/run/ikaros/plugin/starter/ConfigCustom.java
@@ -1,6 +1,7 @@
 package run.ikaros.plugin.starter;
 
 import run.ikaros.api.custom.Custom;
+import run.ikaros.api.custom.Name;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +15,8 @@ import lombok.NoArgsConstructor;
 @Custom(group = "starter.ikaros.run", version = "v1alpha1",
     kind = "Config", singular = "config", plural = "configs")
 public class ConfigCustom {
+    @Name
+    private String name;
     private String field1;
     private String field12;
 }
