e927f604d52d41963fbe396a2a4203fee949515d
chivehao <chivehao@ikaros.run>
2024-09-24 06:49:40 +0800

    feat: add backend task submit code.

--- src/main/java/run/ikaros/plugin/starter/StarterPlugin.java
+++ src/main/java/run/ikaros/plugin/starter/StarterPlugin.java
@@ -4,19 +4,48 @@ package run.ikaros.plugin.starter;
 import lombok.extern.slf4j.Slf4j;
 import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
+import run.ikaros.api.core.task.TaskOperate;
 import run.ikaros.api.plugin.BasePlugin;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @Slf4j
 @Component
 public class StarterPlugin extends BasePlugin {
+    private final TaskOperate taskOperate;
 
-    public StarterPlugin(PluginWrapper wrapper) {
+    public StarterPlugin(PluginWrapper wrapper, TaskOperate taskOperate) {
         super(wrapper);
+        this.taskOperate = taskOperate;
     }
 
     @Override
     public void start() {
         log.info("plugin [PluginStarter] start success");
+        // submitParallelTask();
+    }
+
+    private void submitParallelTask() {
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            taskOperate.submit(getWrapper().getPluginId() + "-parallel-" + i,
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            log.info("Submit task with index:{}", finalI);
+                            try {
+                                Thread.sleep(new Random().nextLong(0, 5000));
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            log.info("Finish task with index:{}", finalI);
+
+                        }
+                    })
+                    .subscribe();
+        }
     }
 
     @Override
